8d1eec503cadbaa31ab75f692ef817eb986fde2d
joacodia619 <joaquind2013@gmail.com>
2022-08-18 10:47:01 -0300

    Mira

--- js/stadiums.js
+++ js/stadiums.js
@@ -1,86 +1,44 @@
-let options = document.getElementById("options");
-let optionList = ["Ahmad bin Ali", "Al Bayt, Al Janoub", "Al Thumama", "Education City", "Khalifa International", "Lusail", "Ras Abu Abound"];
+let optionList = [];
+class seleccion {
+  constructor(name, capacity, imagen, city) {
+    this.name = name;
+    this.capacity = capacity;
+    this.imagen = imagen;
+    this.city = city;
+  }
+}
+optionList.push(new estadio("Ahmad bin Ali", "50.000", "../images/AHMED\ BIN\ ALI.png", "Rayan"))
+optionList.push(new estadio("Al Bayt", "60.000", "../images/Al-Bayt-Stadium.png", "Jor"))
+optionList.push(new estadio("Al Janoub", "40.000", "../images/AL\ JANOUB.png", "Al Wakrah"))
+optionList.push(new estadio("Al Thumama", "40.000", "../images/AL\ THUMAMA.png", "Doha"))
+optionList.push(new estadio("Education City", "45.350", "../images/CIUDAD\ DE\ LA\ EDUCACIÓN.png", "Ar Rayyan"))
+optionList.push(new estadio("Khalifa International", "50.000", "../images/INTERNACIONAL\ KHALIFA.png", "Al Waab St"))
+optionList.push(new estadio("Lusail", "80.000", "../images/ICÓNICO\ DE\ LUSAIL.png", "Lusail"))
+optionList.push(new estadio("Ras Abu Abound", "40.000", "../images/Estadio\ 974.png", "Doha"))
+
 let button = document.getElementById("button");
+let containerStadiums = document.getElementById("containerStadiums")
+containerStadiums.addEventListener('click', x => {
+  // let value = .getAttribute('value')
+})
+// button.addEventListener('click', e => {
+//   let grupo = document.getElementById('inputGrupo').value
+//   let seleccionesDeGrupo = seleccionesDelMundial.filter(x => x.grupo == grupo)
+//   let cardDeCartaEnIngles = ''
+//   seleccionesDeGrupo.forEach(x => {
+//     const { imagen, pais, cups, bestPlayer } = x
+//     cardDeCartaEnIngles += `
+//       <div class="card">
+//         <div class="front">
+//           <img src="${imagen}" alt="">
+//         </div>
+//         <div class="back">
+//           <h1>${pais}</h1>
+//           <h2>${cups} world cups</h2>
+//           <p>Best player ever: ${bestPlayer}</p>
+//         </div>
+//       </div>`
+//     document.getElementById('selecciones').innerHTML = cardDeCartaEnIngles
+//   });
 
-let isOpen = false;
-
-button.addEventListener("click", getMessage);
-options.addEventListener("click", addToUIOptions);
-
-function getMessage() {
-  let message = document.createElement("div");
-  message.className = "message";
-
-  if (options.firstElementChild.classList.contains("hide-option")) {
-    message.classList.add("danger");
-    message.textContent = "Oh no! It is cannot be blank";
-
-    document.body.appendChild(message);
-
-    deleteMessage(message);
-  }
-  else {
-    message.classList.add("success");
-    message.textContent = "Thanks for completing ";
-
-    document.body.appendChild(message);
-
-    deleteMessage(message);
-  }
-
-}
-
-function deleteMessage(el) {
-  setTimeout(() => {
-    document.body.removeChild(el);
-  }, 6000);
-}
-
-function addToUIOptions(e) {
-  if (e.target.classList.contains("hide-option")) {
-    controlOptions(e);
-  }
-  else {
-    const pickedOption = e.target;
-
-    if (options.firstElementChild.classList.contains("hide-option")) {
-      options.removeChild(options.firstElementChild);
-    }
-    options.insertAdjacentElement("afterbegin", pickedOption);
-
-    deleteOptions();
-    controlOptions(e);
-  }
-}
-
-function controlOptions(e) {
-  if (isOpen === false) {
-    createOptions();
-    options.classList.add("opened");
-    isOpen = true;
-  }
-  else {
-    deleteOptions();
-    options.classList.remove("opened");
-    isOpen = false;
-  }
-}
-
-function deleteOptions() {
-  while (options.childElementCount > 1) {
-    options.removeChild(options.lastElementChild);
-  }
-}
-
-function createOptions() {
-  optionList.forEach(element => {
-    if (options.firstElementChild.textContent !== element) {
-      let option = document.createElement("div");
-      option.className = "option";
-      option.textContent = element;
-
-      options.firstElementChild.insertAdjacentElement("afterend", option);
-    }
-  });
-};
-console.log(...optionList)
+// })
